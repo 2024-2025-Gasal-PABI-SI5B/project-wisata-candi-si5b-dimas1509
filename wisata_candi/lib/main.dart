ed2b46af4e12896ef84d0e70e1b6cd90d2d36fb6
dimas1509 <mdimasdimas0915@gmail.com>
2024-11-14 10:01:42 +0700

    m.dimas project Candi

--- wisata_candi/lib/main.dart
+++ wisata_candi/lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:wisata_candi/data/candi_data.dart';
 import 'package:wisata_candi/screens/detail_screen.dart';
+import 'package:wisata_candi/data/candi_data.dart';
 
-
-void runApp(MainApp mainApp) {
+void main() {
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
@@ -14,12 +14,10 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       title: 'Wisata Candi',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: DetailScreen(
-        candi: candiList[0],
-      ),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true),
+      //home: DetailScreen(candi: candiList[0]),
+      home: DetailScreen(candi: candiList[0]),
     );
   }
 }
--- wisata_candi/lib/screens/detail_screen.dart
+++ wisata_candi/lib/screens/detail_screen.dart
@@ -1,56 +1,77 @@
 import 'package:flutter/material.dart';
 import 'package:wisata_candi/models/candi.dart';
 
-class DetailScreen extends StatelessWidget{
-  final  Candi candi;
+class DetailScreen extends StatelessWidget {
+  final Candi candi;
 
   const DetailScreen({super.key, required this.candi});
 
-
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child : Image.asset(
-                candi.imageAsset,
-                width: double.infinity,
-                height: 300,
-                fit :BoxFit.cover,
+          // DETAIL HEADER
+          Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset(
+                    candi.imageAsset,
+                    width: double.infinity,
+                    height: 300,
+                    fit: BoxFit.cover,
+                  ),
+                ),
               ),
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: Container(
+                    decoration: BoxDecoration(
+                    color: Colors.deepPurple[100]?.withOpacity(0.8),
+                    shape: BoxShape.circle,
+                  ),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.arrow_back),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          
+          // DETAIL INFO
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Info atas
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                    candi.name,
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                    ),
+                    ),
+                    IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.favorite_border),
+                    ), // Icon button
+                  ],
+                ),
+                // Info tengah
+                // info bawah
+              ],
             ),
           ),
-          // Tombol Back Custom 
-          Padding(
-            padding: const EdgeInsets.all(16),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.deepPurple[100]?.withOpacity(0.0),
-                shape: BoxShape.circle,
-              ),
-              child: IconButton(
-                onPressed: () {}, icon: const Icon(Icons.arrow_back)),
-              ),
-            ),
-          ],
+        ],
       ),
-      // Detail Info 
-      Padding (
-        padding: EdgeInsets.symmetric(horizontal: 16),
-        child: Column{
-          children: [
-            // info atas 
-            Row(
-              children:
-              MainAxisAlignment:mainAxisAlignment.spaceBetween,
-                Text(candi.name),
-                IconButton(onPressed: (){}), icon: icon(/Icons.favorite_)
-              ],
-            )
-          
-        }
-        
+    );
+  }
+}
