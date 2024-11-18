32e034975bde170260e966176e656015d041cb20
dimas1509 <mdimasdimas0915@gmail.com>
2024-11-18 07:16:56 +0700

    finish detail screen

--- wisata_candi/lib/screens/detail_screen.dart
+++ wisata_candi/lib/screens/detail_screen.dart
@@ -1,4 +1,4 @@
-
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:wisata_candi/models/candi.dart';
 
@@ -133,29 +133,60 @@ class DetailScreen extends StatelessWidget {
                 const SizedBox(
                   height: 10,
                 ),
-                SizedBox(),
-                const SizedBox(
-                  height: 4,
+                SizedBox(
+                  height: 100,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: candi.imageUrls.length,
+                    itemBuilder:(context, index){
+                      return Padding(
+                        padding: const EdgeInsets.only(right: 8),
+                        child: GestureDetector(
+                          onTap: () {},
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              border: Border.all(
+                                color: Colors.deepPurple.shade100,
+                                width: 2,
+                              )),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: CachedNetworkImage(
+                              imageUrl: candi.imageUrls[index],
+                              height: 120,
+                              width: 120,
+                              fit: BoxFit.cover,
+                              placeholder: (context, url) => Container(
+                                width: 120,
+                                height: 120,
+                                color: Colors.deepPurple[50],
+                              ),
+                              errorWidget: (context, Uri, Error) => 
+                                  const Icon(Icons.error),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                    const SizedBox(
+                    height: 4,
+                    ),
+                    const Text(
+                    'Tap untuk Memperbesar',
+                    style: TextStyle(fontSize: 12, color: Colors.black54),
+                    ),
+                  ],
                 ),
-                Text('Tap untuk Memperbesar', style: TextStyle(fontSize: 12, color: Colors.black54),
-              ],
-            ) ,
-          )
-        ],      
-                  ),
-                  
-                ),
-                
-        
-                
-                  
-                
-                ],
               ),
-      ),
+            ],      
           ),
-        ],
+        ),
       ),
     );
   }
 }
+    
