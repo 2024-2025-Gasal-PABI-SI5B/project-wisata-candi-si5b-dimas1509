849e50e57d7121c6fa42d9193bda2f05ab6b7333
dimas1509 <mdimasdimas0915@gmail.com>
2024-12-02 07:34:46 +0700

    quarter project

---
+++ wisata_candi/lib/data/user_data.dart
@@ -0,0 +1,6 @@
+import 'package:wisata_candi/models/user.dart';
+
+var userList = [
+  User(nama: 'Dimas Saputra', email: 'dimassaputra@gmail.com', password: '123456'),
+  User(nama: 'Yunisarianaputri', email: 'yunisaputri@gmail.com', password: '67800'),
+];
---
+++ wisata_candi/lib/models/user.dart
@@ -0,0 +1,9 @@
+class User {
+  final String nama;
+  final String email;
+  final String password;
+
+  User({required this.nama, required this.email, required this.password});
+
+  
+}
